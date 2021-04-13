caab07bb57737e28e2a49cdb183781ef17ee609e
Victoria <vika.sirghii@gmail.com>
2021-04-13 19:18:11 +0200

    dinamic nav-menu

---
+++ src/context.js
@@ -0,0 +1,43 @@
+import React, { useState, useContext } from 'react'
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+  const [isSidebarOpen, setSideBarOpen] = useState(false);
+  const [isSubmenuOpen, setSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({page: "", links: []})
+
+  const openSidebar = () => {
+    setSideBarOpen(true)
+  }
+  const closeSidebar = () => {
+    setSideBarOpen(false)
+  }
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page)
+    setLocation(coordinates);
+    setSubmenuOpen(true)
+  }
+  const closeSubmenu = () => {
+    setSubmenuOpen(false)
+  }
+
+  return <AppContext.Provider 
+    value={{isSubmenuOpen, 
+          isSidebarOpen,
+          openSubmenu,
+          openSidebar, 
+          closeSubmenu, 
+          closeSidebar,
+          location,
+          page}}>
+    {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalContext = () =>{
+  return useContext(AppContext)
+}
